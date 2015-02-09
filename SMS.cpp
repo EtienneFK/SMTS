f5285b3ea2ad8b321cd668b2d1860ebe41fee2bb
EtienneFK <fournel.etienne@gmail.com>
2015-02-09 16:08:20 +0100

    blalazjqslkdjbf

--- SMS.cpp
+++ SMS.cpp
@@ -1,62 +1,70 @@
-#include "SMS.h"
+#include "sms.h"
 #include <string>
-using namespace std;
-/* Constructor
-*/
-SMS::SMS(){ 
-	string _message = NULL;
-	char _numTel[10];
-}
-/* Destructor
-*/
-SMS::~SMS(){ 
-}
-/* Send a SMS
-*/
-void SMS::sendSMS(string _message){
-	while(!LSMS.ready()){
-		delay(1000);
-	}
-	LSMS.beginSMS(_numtel);
-	LSMS.print(_message);
-	LSMS.endSMS()
-}
-/* Return SMS into a char*
-*/
-char *SMS::receiveSMS(){
 
-	while(!LSMS.ready()){
-		delay(1000);
+Sms::Sms() {
+	string _sendMessage = NULL;
+	char _numTel[10];
 	}
-	char _buf[10];
-	if(LSMS.available()){							//Check if there a SMS available
-		
-	LSMS.remoteNumber(_buf,10); 					//Number stored
-		if(strcmp(_buf,_numTel)=0){ 				//Verify if the number is allowed to communicate with the device
-			char _message[50]; 						//Message received
-			int c; 									//Chars of SMS
-			int n; 									//Increment
-			while(true){
-			c = LSMS.read(); 						//Message content (one byte at a time)
-			
-			if(c<0)
-				break; 								//End of message content
-			}
-		
-			LSMS.flush();							//Delete message
-			}
-		
-	}
+
+Sms::~Sms() {
 	
-	return NULL;
-}
-/* Return true if the network is sufficient for sending a SMS otherwise false
-*/
-boolean SMS::checkGSMAvailability(){
-	return true;
-}
-/* A voir
-*/
-int SMS::errorSMS(){
+	}
+
+int Sms::sendSms(string _message) {
+	int error = 0;
+	while(!LSMS.ready()) {
+		delay(1000);
+		error++;
+		if(error >= 10) return error;
+		}
+
+	LSMS.beginSms(_numtel);
+	LSMS.print(_sendMessage);
+	LSMS.endSms();
+
 	return 0;
-}
+	}
+
+char *Sms::receiveSms() { // retourne le premier Sms reçu
+	char *_message = new char[50];	*(_message+0) = '\0'; // Message received
+	int c;			// Chars of Sms
+	int n=0;		// Increment
+	char _buf[10];
+
+	while(!LSMS.ready()) {
+		delay(1000);
+		}
+	if(LSMS.available()){
+		LSMS.remoteNumber(_buf,10);	// Number stored
+
+		if(strcmp(_buf,_numTel) == 0) {	// Verify if the number is allowed for communicate with the device
+			while(true) {
+				c = LSMS.read(); // Message content (one byte at a time)
+				if(c <= 0) break;	// End of message content
+				else { *(_message + n) = (char)c; n++; }
+				}	
+			}
+		return _message;
+		}
+	return NULL;
+	}
+
+char *Sms::dernierSms() { // retourne le Sms le plus récent
+	char *buf;
+	int t = 0, prect = 0, intervalle = 0, com = 0, cardio = 0, temp = 0;
+
+	buf = receiveSms();
+	sscanf(buf, "%d %d %d %d %d", &prect, &intervalle, &cardio, &temp, &com);
+	
+	while(buf != NULL) {
+		buf = receiveSms();
+		sscanf(buf, "%d", &t);
+
+		if(t > prect) {
+			sscanf(buf, "%d %d %d %d %d", &prect, &intervalle, &cardio, &temp, &com);
+			}
+		}
+
+	return buf;
+	}
+
--- SMS.h
+++ SMS.h
@@ -1,23 +1,15 @@
-/* Classe de gestion des échanges SMS
-	Class who manage SMS data exchange
-*/
 #ifndef DEF_SMS
 #define DEF_SMS
 
-#include <LGSM.h>
+#include <GSM.h>
 
-class SMS{
-	private:
-	GSM gsmAccess;
-	GSM_SMS sms;
-	
-	
+class SMS {
 	public:
-	SMS(); //Constructor
-	~SMS(); //Destructor
-	void sendSMS(string _message); //send a SMS
-	char *receiveSMS(); //return SMS into a char*
-	bool checkGSMAvailability(); //return true if the network is sufficient for sending a SMS otherwise false
-	int errorSMS(); //A voir
-}
+		SMS();
+		~SMS();
+		void sendSMS(string _message);
+		char *receiveSMS();
+		char *dernierSMS();
+	}
+
 #endif
